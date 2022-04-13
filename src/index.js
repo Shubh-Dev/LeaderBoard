643f46a26a66184f44c1ad5ed9bff8d261962ce3
Shubh <shubh.scb@gmail.com>
2022-04-13 21:56:33 +0530

    get data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './style.css';
-import addDetails from '../modules/class.js';
+// import addDetails from '../modules/class.js';
+// addDetails();
 
-addDetails();
+
+const addBtn = document.querySelector('.submit-btn');
+const mainDynamicBox = document.querySelector('.dynamic-box');
+
+function fetchAndDisplay() {
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+    fetch(url).then((response) => {return response.json()}).then((data) => {
+       mainDynamicBox.innerHTML = data;
+       console.log(data);
+    });
+};
+
+addBtn.addEventListener('click', () => {
+    fetchAndDisplay();
+})
+
